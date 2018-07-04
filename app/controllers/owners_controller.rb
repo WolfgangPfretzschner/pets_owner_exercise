241653d5b3cf5e6a98bf50d0a0de766b3723de48
Wolfgang Pfretzschner <wolfgangp@live.com>
2018-07-04 11:57:52 -0400

    setup resources

---
+++ app/controllers/owners_controller.rb
@@ -0,0 +1,30 @@
+class OwnersController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+    #code
+  end
+
+  def create
+    #code
+  end
+
+  def show
+    #code
+  end
+
+  def edit
+    #code
+  end
+
+  def update
+    #code
+  end
+
+  def destroy
+
+  end
+end
